eef71cb161c67389348933f559c1057c44cac516
Juan Pablo Vanegas <juan.vanegas@tektontech.com>
2020-08-04 13:25:50 -0500

    Refactor: Change constants to preprocessor defines

--- code/c++/constants.h
+++ code/c++/constants.h
@@ -4,13 +4,16 @@
 #include<string>
 #include<sstream>
 
-const int cube = 64;
-const int Lx = cube, Ly = cube, Lz = cube;
-const int D = 3, Q = 19;
+#define cube 64
+#define Lx 256
+#define Ly cube
+#define Lz cube
+#define D 3
+#define Q 19
 
-const int size = Lx*Ly*Lz*Q;
-const int x_mult = Ly*Lz*Q;
-const int y_mult = Lz*Q;
-const int z_mult = Q;
+#define size (Lx*Ly*Lz*Q)
+#define x_mult (Ly*Lz*Q)
+#define y_mult (Lz*Q)
+#define z_mult Q
 
 const double tau = 0.55, Utau = 1.0/tau, UmUtau = 1-Utau;
