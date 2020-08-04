de43d819a39e6f586e4e4d61a889fda0bcf34e9d
Juan Pablo Vanegas <juan.vanegas@tektontech.com>
2020-08-04 18:22:07 -0500

    Fix: Minor bugs

--- code/cuda/main.cu
+++ code/cuda/main.cu
@@ -3,12 +3,13 @@
 int main(void){
     LatticeBoltzmann Bogota;
     int t_max = 10;
+    float v_wind = 0.1;
 
-    Bogota.initialize(1.0, 1.0, 0, 0);
+    Bogota.initialize(1.0, 0, 0, 0);
 
     for(int t=0; t<t_max; t++){
         Bogota.collide();
-        Bogota.impose_fields();
+        Bogota.impose_fields(v_wind);
         Bogota.propagate();
     }
 
