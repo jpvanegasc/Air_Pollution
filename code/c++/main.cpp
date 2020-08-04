d0e2d0999303dae57668f8286e6ec403c9362a95
Juan Pablo Vanegas <juan.vanegas@tektontech.com>
2020-08-04 11:50:13 -0500

    Feat: main.cpp update

--- code/c++/main.cpp
+++ code/c++/main.cpp
@@ -5,16 +5,18 @@ std::string filename(int t);
 int main(void){
     LatticeBoltzmann Bogota;
     int t_max = 100;
-    double rho0 = 1.0, v = 10.0;
+    double rho0 = 1.0, v = 0.1;
 
     Bogota.initialize(rho0, 0, 0, 0);
 
     for(int t=0; t<t_max; t++){
         Bogota.collide();
-        Bogota.impose_fields(v);
         Bogota.propagate();
+        Bogota.impose_fields(v);
     }
-    Bogota.save(filename(0), 1);
+
+    Bogota.save(filename(1), v);
+
     return 0;
 }
 
