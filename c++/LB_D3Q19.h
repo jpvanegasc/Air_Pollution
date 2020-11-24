cc700fc62727694358eaa21b7fee7b91f493d269
Juan Pablo Vanegas <juan.vanegas@tektontech.com>
2020-11-24 18:18:11 -0500

    Refactor: Move constants to LB file

--- c++/LB_D3Q19.h
+++ c++/LB_D3Q19.h
@@ -1,7 +1,43 @@
-#include "constants.h"
+#ifndef __LB_CPP_LB_D3Q19_H
+#define __LB_CPP_LB_D3Q19_H
 
-#ifndef __AIR_POLLUTION_CPP_LB_D3Q19_H
-#define __AIR_POLLUTION_CPP_LB_D3Q19_H
+#include<iostream>
+#include<cmath>
+#include<fstream>
+#include<string>
+#include<sstream>
+
+// Geometry
+#define Lx 256
+#define Ly 64
+#define Lz 64
+#define D 3
+#define Q 19
+
+// Obstacle
+#define Lx3 (Lx/3)
+#define Ly3 (Ly/3)
+#define Lz2 (Lz/2)
+
+#define T_Lx3 (2*Lx/3)
+#define T_Ly3 (2*Ly/3)
+
+// 3D to 1D
+#define size (Lx*Ly*Lz*Q)
+#define x_mult (Ly*Lz*Q)
+#define y_mult (Lz*Q)
+#define z_mult Q
+
+/**
+ * Transform from 3D notation to 1D notation 
+ * @return 1D macro-coordinate on array
+ */
+#define get_1D(ix, iy, iz) ((ix*x_mult) + (iy*y_mult) + (iz*z_mult))
+
+// LB constants
+#define tau 0.55
+#define Utau (1.0/tau)
+#define UmUtau (1.0-Utau)
 
 class LatticeBoltzmann{
     protected:
