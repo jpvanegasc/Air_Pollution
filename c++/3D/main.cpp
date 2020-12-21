4e696787b115d5583a584392573b7664f7a5fbb2
Juan Pablo Vanegas <juan.vanegas@tektontech.com>
2020-12-20 22:44:30 -0500

    Feat: Update main.cpp

--- c++/3D/main.cpp
+++ c++/3D/main.cpp
@@ -2,9 +2,17 @@
 
 #include "LB_D3Q19.h"
 
-int main(void){
-    LatticeBoltzmann3D Diffusion;
+#define TMAX 100
 
-    
+int main(void){
+    LatticeBoltzmann3D Fluids;
+
+    Fluids.initialize();
+
+    for(int t=0; t<TMAX; t++){
+        Fluids.collide();
+        Fluids.stream();
+    }
+
     return 0;
 }
